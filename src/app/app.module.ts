aeab6bcc48d33d672a1547bc8c22702bc7e917b7
Christian von Rohr <ch.vrohr@gmail.com>
2019-09-20 14:06:48 +0200

    Dynamic Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,8 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
+import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 
 @NgModule({
   declarations: [
@@ -39,7 +41,9 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     RecipeEditComponent,
     CropPipe,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent,
+    PlaceholderDirective
   ],
   imports: [
     BrowserModule,
@@ -49,6 +53,7 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     HttpClientModule
   ],
   providers: [ ShoppingListService, RecipeService, { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true } ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  entryComponents: [ AlertComponent ]
 })
 export class AppModule { }
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,21 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ComponentFactoryResolver, ViewChild, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService, AuthResponseData } from './auth.service';
 import { finalize } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent implements OnInit, OnDestroy {
+
+  @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
+
   isLoginMode = true;
   isLoading = false;
   error: string = null;
 
-  constructor(private authService: AuthService, private router: Router) {}
+  private closeSub: Subscription;
+
+  constructor(private authService: AuthService, private router: Router, private cfr: ComponentFactoryResolver) {}
 
   ngOnInit() {}
 
@@ -40,21 +47,46 @@ export class AuthComponent implements OnInit {
       authObs = this.authService.signup(email, password);
     }
 
-    authObs.pipe(
-      finalize(() => {
-        this.isLoading = false;
-      })
-    )
-    .subscribe({
-      next: response => {
-        console.log(response, 'signed up');
-        form.reset();
-        this.router.navigate([ '/recipes' ]);
-      },
-      error: error => {
-        this.error = error;
-        console.warn(this.error, 'error on signup');
-      }
+    authObs
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+        })
+      )
+      .subscribe({
+        next: response => {
+          console.log(response, 'signed up');
+          form.reset();
+          this.router.navigate(['/recipes']);
+        },
+        error: error => {
+          this.error = error;
+          this.showErrorAlert(error);
+          console.warn(this.error, 'error on signup');
+        }
+      });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  private showErrorAlert(error: string) {
+    const alertCmptFactory = this.cfr.resolveComponentFactory(AlertComponent);
+    const viewContainerRef = this.alertHost.viewContainerRef;
+    viewContainerRef.clear();
+
+    const componentRef = viewContainerRef.createComponent(alertCmptFactory);
+    componentRef.instance.message = error;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      viewContainerRef.clear();
     });
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub) {
+      this.closeSub.unsubscribe();
+    }
   }
 }
