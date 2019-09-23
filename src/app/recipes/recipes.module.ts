4ae7d3e03b72750dcc06300b764367207292d4ed
Christian von Rohr <ch.vrohr@gmail.com>
2019-09-23 16:40:27 +0200

    moved to separate modules #2 and added lazy route loading

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { RecipesComponent } from './recipes.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
@@ -11,6 +11,7 @@ import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
 import { RecipesRoutingModule } from './recipes-routing.module';
 import { CropPipe } from '../shared/crop.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -24,8 +25,9 @@ import { CropPipe } from '../shared/crop.pipe';
   ],
   imports: [
     RouterModule,
-    CommonModule,
+    SharedModule,
     ReactiveFormsModule,
+    FormsModule,
     RecipesRoutingModule
   ],
   exports: [
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -6,12 +6,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 
-const routes: Routes = [
-  { path: 'shopping-list', component: ShoppingListComponent },
-];
+const routes: Routes = [{ path: '', component: ShoppingListComponent }];
 
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingEditComponent],
-  imports: [RouterModule.forChild(routes), CommonModule, FormsModule]
+  imports: [ RouterModule.forChild(routes), CommonModule, FormsModule ],
+  exports: [ RouterModule ]
 })
 export class ShoppingListModule {}
